fa631aff9c217dd2272e5422a96aa4617f836b62
이해빈 <>
2020-12-14 19:01:43 +0900

    .

---
+++ onm-vue/src/components/dashboard/platform/iotGwStatus.vue
@@ -0,0 +1,113 @@
+<style src="../../../css/body.css"></style>
+
+<template>
+  <div>
+    <p style="font-size:30px; font-weight:bold">{{ title }}</p>
+
+<!-- search -->
+    <table class="search">
+      <tr>
+        <td class="header">국사코드 : </td>
+        <td><input></td>
+        <td class="buttons" style="align:right" ><button>검색</button></td>
+      </tr>
+    </table>
+
+<!-- contents -->
+    <table class="contents">
+      <tr>
+        <th>국사코드</th>
+        <th>전체</th>
+        <th>정상</th>
+        <th>개통 대기</th>
+        <th>개통 진행</th>
+        <th>개통 실패</th>
+        <th>네트워크 장애</th>
+      </tr>
+       <tr>
+        <td>1</td>
+        <td>68</td>
+        <td>5</td>
+        <td>56</td>
+        <td>453</td>
+        <td>88</td>
+        <td>44</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>33</td>
+        <td>345</td>
+        <td>23</td>
+        <td>123</td>
+        <td>14</td>
+        <td>56</td>
+      </tr>
+      <tr>
+        <td>3</td>
+        <td>345</td>
+        <td>54</td>
+        <td>124</td>
+        <td>23423</td>
+        <td>2342</td>
+        <td>345</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>234</td>
+        <td>34</td>
+        <td>2323</td>
+        <td>343</td>
+        <td>123</td>
+        <td>13</td>
+      </tr>
+      <tr>
+        <td>5</td>
+        <td>234</td>
+        <td>56</td>
+        <td>88</td>
+        <td>67</td>
+        <td>55</td>
+        <td>22</td>
+      </tr>
+    </table>
+
+<!-- pager -->
+  <ul class="pagination">
+  <!-- li태그의 클래스에 disabled를 넣으면 마우스를 위에 올렸을 때 클릭 금지 마크가 나오고 클릭도 되지 않는다.-->
+  <!-- disabled의 의미는 앞의 페이지가 존재하지 않다는 뜻이다. -->
+    <li class="disabled">
+      <a href="#">
+        <span>«</span>
+      </a>
+    </li>
+    <!-- li태그의 클래스에 active를 넣으면 색이 반전되고 클릭도 되지 않는다. -->
+    <!-- active의 의미는 현재 페이지의 의미이다. -->
+    <li class="active"><a href="#">1</a></li>
+    <li style="color:black"><a href="#">2</a></li>
+    <li><a href="#">3</a></li>
+    <li><a href="#">4</a></li>
+    <li><a href="#">5</a></li>
+    <li>
+      <a href="#">
+        <span>»</span>
+      </a>
+    </li>
+  </ul>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'IoT GW 상태 현황'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  
+</style>
---
+++ onm-vue/src/components/dashboard/platform/streamerStatusPopup.vue
@@ -0,0 +1,83 @@
+<style src="../../../css/body.css"></style>
+
+<template>
+  <div>
+    <p style="font-size:30px; font-weight:bold">{{ title }}</p>
+
+    <table class="pop">
+      <tr>
+        <th>Wowza 서버 INDEX : </th> <td><input></td>
+        <th>RTSP 포트 : </th> <td><input></td>
+      </tr>
+      <tr>
+        <th>프로토콜 : </th> <td><input></td>
+        <th>RTMP 포트 : </th> <td><input></td>
+      </tr>
+      <tr>
+        <th>사설 IP : </th> <td><input></td>
+        <th>RTMPS 포트 : </th> <td><input></td>
+      </tr>
+      <tr>
+        <th>공인 IP : </th> <td><input></td>
+        <th>WOWZA URL : </th> <td><input></td>
+      </tr>
+      <tr>
+        <th>스트림 주소 : </th> <td><input></td>
+        <th>WOWZA 상태 : </th> <td><input></td>
+      </tr>
+      <tr>
+        <th>포트 : </th> <td><input></td>
+        <th>GLS 여부: </th> <td><select><option value="Y">Y</option><option value="N">N</option></select></td>
+      </tr>
+      <tr>
+        <th>계정 : </th> <td><input></td>
+        <th>GLS RTC 포트 : </th> <td><input></td>
+      </tr>
+      <tr>
+        <th>패스워드 : </th> 
+        <td colspan="3"><input></td>
+      </tr>
+      <tr>
+        <td colspan="4" style="text-align:center">
+          <router-link to="/platform/streamer"><button>완료</button></router-link>
+          <router-link to="/platform/streamer"><button>취소</button></router-link>
+        </td>
+      </tr>
+    </table>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '스트리머 관리'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+table.pop tr th {
+  background :#cecece;
+  text-align: right;
+  font-weight: normal;
+}
+
+table.pop tr td {
+  background :#cecece;
+  text-align: left;
+  padding : 10px;
+}
+
+table.pop{
+  background : #cecece;
+  padding : 10px 10px 10px 10px;
+  margin : auto;
+  margin-bottom: 100px;
+}
+
+</style>
