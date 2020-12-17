a08d3e7df244e576c97ad22dcea4216b6c91b45e
이정환 <jhlee51047@naver.com>
2020-12-17 19:49:38 +0900

    commit to porting

---
+++ onm-vue/src/components/dashboard/usrMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>{{ title }}</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '사용자 정보 조회 메인'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
