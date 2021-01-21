f07dac587759cf13a23ec20bb6a8051b499ec9c6
이해빈 <>
2021-01-21 13:49:45 +0900

    플랫폼 현황

--- onm-vue/src/components/dashboard/platform/iotgw/iotgwList.vue
+++ onm-vue/src/components/dashboard/platform/iotgw/iotgwList.vue
@@ -32,12 +32,13 @@ export default {
       return {
         headers: [
           { text: '국사코드', value: 'local_gw_id' },
-          { text: '전체', value: 'cam_cnt' },
-          { text: '정상', value: 'status_code' },
-          { text: '개통대기', value: 'waiting_cam_cnt' },
-          { text: '개통진행', value: 'proc_cam_cnt' },
-          { text: '개통실패', value: 'open_fail_cam_cnt' },
-          { text: '네트워크장애', value: 'networkfail_cam_cnt' },
+          { text: '전체', value: 'total_cnt' },
+          { text: '정상', value: 'normal_cnt' },
+          { text: '개통대기', value: 'waiting_cnt' },
+          { text: '개통진행', value: 'proc_cnt' },
+          { text: '개통실패', value: 'disconn_cnt' },
+          { text: '네트워크장애', value: 'fail_cnt' },
+
         ]
       }
     }
