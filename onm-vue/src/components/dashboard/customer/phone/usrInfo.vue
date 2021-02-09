c0e116f050fcf93d6d0cfd669cb5d70f24faef27
이해빈 <>
2021-02-09 17:13:11 +0900

    사용자관리 > 사용자전화번호

--- onm-vue/src/components/dashboard/customer/phone/usrInfo.vue
+++ onm-vue/src/components/dashboard/customer/phone/usrInfo.vue
@@ -1,69 +1,153 @@
 <template>
-
     <v-container fluid>
       <v-card>
-        <!-- <v-toolbar primary dense>
-          <v-toolbar-title>{{ title }}</v-toolbar-title>
-        </v-toolbar> -->
-        <usrInfo-query v-on:search="searchToUsrInfo" title></usrInfo-query>
-        <usrInfo-list v-bind:List=list title></usrInfo-list>
-      </v-card> 
-    </v-container>
+        <query 
+          v-on:search="searchToProcess"
+          v-bind:param="searchParam"
+        ></query>
 
+        <list 
+          v-bind:pList=pList
+          v-bind:resPagingInfo="resPagingInfo"
+          @pagination="searchToProcess"
+        ></list>
+      </v-card>
+    </v-container>
 </template>
 
 <script>
-import UsrInfoList from './usrInfoList'
-import UsrInfoQuery from './usrInfoQuery'
+import List from './usrInfoList'
+import Query from './usrInfoQuery'
 
 import axios from "axios"
 
+const headers = {
+  "User-Agent": "GiGA Eyes (compatible;DeviceType/iPhone;DeviceModel/SCH-M20;DeviceId/3F2A009CDE;OSType/iOS;OSVersion/5.1.1;AppVersion/3.0.0;IpAddr/14.52.161.208)",
+  "Content-Type": "application/json",
+};
+
+const url = `${process.env.VUE_APP_BACKEND_SERVER_URL_TB}/${process.env.VUE_APP_API_VERSION}/ONM_13004/get_user_tel_list`;
+
 export default {
+  
   components: {
-    UsrInfoList,
-    UsrInfoQuery
+    List, Query
   },
+  
   data () {
     return {
-      title: '사용자 전화번호',
-      list: []
-      // pList: [
-      //   {usrId:"0012484660", phoneNumber:"01011101110", numberType: "M", usrName: "홍길동", subId:800124846607000, alarmAuth:"Y", approve:"Y", approveDate:"2020-11-03 02:00:04.173138", statusCode: "S", mgtCode: "S", etc: ""},
-      //   {usrId:"0012484661", phoneNumber:"01011101112", numberType: "S", usrName: "홍길순", subId:800124846607001, alarmAuth:"Y", approve:"Y", approveDate:"2020-11-03 02:00:04.173138", statusCode: "S", mgtCode: "S", etc: ""}
-      // //Legacy octal literals are not allowed in strict mode 로 문자열처리
-      // ]
+      pList: [],
+      reqPagingInfo: {
+        page_no: 1,
+        view_cnt: 10,
+      },
+      resPagingInfo: {},
+      searchParam: {
+        start_date: "",
+        end_date: "",
+        user_name: "",
+        user_id: "",
+        tel_no: "",
+      },
+      
     }
-  },
-  created: function(){
-    axios
-      .get(`${process.env.VUE_APP_BACKEND_SERVER_URL}/get_user_tel_list`)
-      .then((result) => {
-        console.log(result)
-        // this.list = JSON.parse(result.data.menu)
-        this.list = result.data
-      })
-      .catch((ex) => {
-        console.log('조회 실패',ex)
-      })
   },
   methods: {
-    searchToUsrInfo: function(params){
-      console.log("부모 메소드 searchToButton 호출: "+JSON.stringify(params));
-      console.log(process.env);
+    searchToProcess: function(params){
+
+      var reqParams = this.handleParams(params);
+
       axios
-            .post(`${process.env.VUE_APP_BACKEND_SERVER_URL}/customer-phone/query`, {       
-                  params
-            })
-            .then((result) => {
-              console.log(result)
-              // this.list = JSON.parse(result.data.menu)
-              this.list = result.data
-            })
-            .catch((ex) => {
-              console.log('조회 실패',ex)
-            })
-    }
+      .post(url, reqParams, headers)
+      .then( (response) => {
+
+        var resCode = response.data.res_code;
+        var resMsg = response.data.res_msg;
+        if (resCode == 200) {
+          this.pList = response.data.data.tel_no_list;
+          this.resPagingInfo = response.data.data.paging_info;
+        } else {
+          this.pList = [];
+          this.resPagingInfo = {};
+          alert(resCode + " / " + resMsg);
+        }
+
+      })
+      .catch(function (error) {
+        console.log(error);
+        alert("Error")
+      })
+      .finally(function () {
+        // always executed
+      });
+
+    },
+
+    handleParams: function (params) {
+      console.log(params);
+      let newParams = {};
+      if (params.page === undefined || params.page === "") {
+        newParams.page_no = this.reqPagingInfo.page_no;
+      } else {
+        newParams.page_no = params.page;
+      }
+
+      if (params.itemsPerPage === undefined || params.itemsPerPage === "") {
+        newParams.view_cnt = this.resPagingInfo.view_cnt;
+      } else {
+        newParams.view_cnt = params.itemsPerPage;
+      }
+
+      if (params.start_date !== undefined && params.start_date !== "") {
+        newParams.start_date = params.start_date;
+      } else if (
+        this.searchParam.start_date !== undefined &&
+        this.searchParam.start_date !== ""
+      ) {
+        newParams.start_date = this.searchParam.start_date;
+      }
+
+      if (params.end_date !== undefined && params.end_date !== "") {
+        newParams.end_date = params.end_date;
+      } else if (
+        this.searchParam.end_date !== undefined &&
+        this.searchParam.end_date !== ""
+      ) {
+        newParams.end_date = this.searchParam.end_date;
+      }
+
+      if (params.user_name !== undefined && params.user_name !== "") {
+        newParams.user_name = params.user_name;
+      } else if (
+        this.searchParam.user_name !== undefined &&
+        this.searchParam.user_name !== ""
+      ) {
+        newParams.user_name = this.searchParam.user_name;
+      }
+
+      if (params.user_id !== undefined && params.user_id !== "") {
+        newParams.user_id = params.user_id;
+      } else if (
+        this.searchParam.user_id !== undefined &&
+        this.searchParam.user_id !== ""
+      ) {
+        newParams.user_id = this.searchParam.user_id;
+      }
+
+      if (params.tel_no !== undefined && params.tel_no !== "") {
+        newParams.tel_no = params.tel_no;
+      } else if (
+        this.searchParam.tel_no !== undefined &&
+        this.searchParam.tel_no !== ""
+      ) {
+        newParams.tel_no = this.searchParam.tel_no;
+      }
+
+      return newParams;
+    },
+    
   }
+  
 }
 </script>
 
--- onm-vue/src/components/dashboard/customer/phone/usrInfoList.vue
+++ onm-vue/src/components/dashboard/customer/phone/usrInfoList.vue
@@ -1,68 +1,79 @@
 <template>
-    <!-- <div id="app">
-      <v-app id="inspire">
-          <v-data-table
-              :headers="headers"
-              :items="List"
-              :items-per-page="5"
-                item-key="usrId"
-              class="elevation-1"
-              :style="`background-color: #d6f5e5`"
-          >
-          </v-data-table>
-      </v-app>
-    </div> -->
-    <v-container
-        id="regular-tables"
-        fluid
-        tag="section"
+  <v-container
+      id="regular-tables"
+      fluid
+      tag="section"
     >
-        <base-material-card
-            color="orange"
-            dark
-            icon="mdi-keyboard"
-            title="사용자 전화번호 LIST"
-            class="px-5 py-3"
-            >
-            <v-data-table
-                :headers="headers"
-                :items="pList"
-                class="elevation-1"
-            >          
-            </v-data-table>
-        </base-material-card>
-    </v-container>
+
+    <base-material-card
+        color="orange"
+        dark
+        icon="mdi-keyboard"
+        title="사용자 전화번호 LIST"
+        class="px-5 py-3"
+      >
+
+      <v-data-table
+        :headers="headers"
+        :items="pList"
+        :options.sync="options"
+        :server-items-length="resPagingInfo.total_cnt"
+        class="elevation-1"
+      >
+      </v-data-table>
+
+    </base-material-card>
+  </v-container>
 </template>
+
 
 <script>
 export default {
-    props: ['pList', 'title'],
-    data() {
-      return {
-        headers: [
-          {
-            text: '사용자ID',
-            align: 'start',
-            sortable: false,
-            value: 'usrId',
-          },
-          { text: '전화번호', value: 'phoneNumber'},
-          { text: '전화번호 타입', value: 'numberType' },
-          { text: '사용자 이름', value: 'usrName' },
-          { text: 'Sub ID(전화번호 ID)', value: 'subId' },
-          { text: '알람설정권한(Y/N)', value: 'alarmAuth' },
-          { text: '승인여부', value: 'approve' },
-          { text: '승인날짜', value: 'approveDate' },
-          { text: '상태 코드', value: 'statusCode' },
-          { text: 'mgt 상태코드', value: 'mgtCode' },
-          { text: '비고', value: 'etc' },
-        ]
-      }
+  props: ["pList", "resPagingInfo"],
+  data() {
+    return {
+      headers: [
+
+        { text: '사용자 ID', value: 'user_id' },
+        { text: '전화번호', value: 'tel_no' },
+        { text: '전화번호 타입', value: 'auth' },
+        { text: '사용자 이름', value: 'user_name' },
+        { text: 'Sub ID(전화번호 ID)', value: 'tel_no_id' },
+        { text: '알림설정권한(Y/N)', value: 'alim_auth_yn' },
+        { text: '승인여부', value: 'confirm_yn' },
+        { text: '승인날짜', value: 'confirm_date' },
+        { text: '상태코드', value: 'status_code' },
+        { text: 'Mgt 상태코드', value: 'mgt_status' },
+        { text: '비고', value: 'memo' },
+
+      ],
+        options: {},
+        totalList: 0,
+        loading: true,
+
     }
+  },
+
+  methods: {
+    getDataFromApi() {
+      this.loading = true;
+      this.$emit("pagination", this.options);
+    },
+    
+  },
+
+  watch: {
+    options: {
+      handler() {
+        this.getDataFromApi();
+      },
+      deep: true,
+    },
+  },
+    
 }
-     
 </script>
 
-<style scoped>
+<style>
     
 </style>
