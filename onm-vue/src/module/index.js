3f80577e353a3c94080e91313d09d618c1984554
이정환 <jhlee51047@naver.com>
2021-02-16 20:50:08 +0900

    품질 점검 소스 수정 1차

--- onm-vue/src/module/index.js
+++ onm-vue/src/module/index.js
@@ -9,10 +9,10 @@ const resourceHost = "http://localhost:3000"
 
 const state = {
     //  id: 1,
-    accessToken: null,  //cookie 로 대체
+    accessToken: null, //cookie 로 대체
     isAuthenticated: false,
     menu: [],
-    
+
 }
 
 const getters = {
@@ -21,11 +21,11 @@ const getters = {
 
 const mutations = {
     LOGIN(state, param) {
-        console.log("mutations ============ ")
-        console.log(param)
-        console.log(param.data)
-        console.log(JSON.parse(param.menu))
-        console.log("mutations end ============ ")
+        // console.log("mutations ============ ")
+        // console.log(param)
+        // console.log(param.data)
+        // console.log(JSON.parse(param.menu))
+        // console.log("mutations end ============ ")
         state.accessToken = param.data
         state.isAuthenticated = true
         state.menu = JSON.parse(param.menu)
@@ -42,10 +42,10 @@ const actions = {
         return axios
             .post(`${resourceHost}/login`, { id, password })
             .then(({ data }) => {
-            console.log(JSON.parse(data.menu))
-            commit("LOGIN", data)
-            // 모든 HTTP 요청 헤더에 Authorization 을 추가한다.
-            //axios.defaults.headers.common['Authorization'] = `Bearer ${data}`;
+                // console.log(JSON.parse(data.menu))
+                commit("LOGIN", data)
+                    // 모든 HTTP 요청 헤더에 Authorization 을 추가한다.
+                    //axios.defaults.headers.common['Authorization'] = `Bearer ${data}`;
             })
     },
     LOGOUT({ commit }) {
@@ -54,11 +54,11 @@ const actions = {
 }
 
 export default {
-  strict: process.env.NODE_ENV !== 'production',
-  state: {
-    ...state
-  },
-  getters,
-  mutations,
-  actions
+    strict: process.env.NODE_ENV !== 'production',
+    state: {
+        ...state
+    },
+    getters,
+    mutations,
+    actions
 }
