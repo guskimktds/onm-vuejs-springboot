eda30c4af381d428975e310465797c1e4fde33e1
이해빈 <>
2021-03-04 14:43:23 +0900

    Proxy 제거

--- onm-vue/vue.config.js
+++ onm-vue/vue.config.js
@@ -1,15 +1,4 @@
 module.exports = {
-  transpileDependencies: ['vuetify'],
-  // 개발 서버 설정
-  devServer: {
-    // 프록시 설정
-    proxy: {
-      // 프록시 요청을 보낼 api의 시작 부분
-      '/V110': {
-        // 프록시 요청을 보낼 서버의 주소
-        target: 'https://test-onm.ktvsaas.co.kr:8443'
-      }
-    }
-  }
+  transpileDependencies: ['vuetify']
 }
 
