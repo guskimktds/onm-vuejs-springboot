8b8e9ca586aca859342bdc9441bc1d1d4a4a96c4
DESKTOP-CQKCS6R\KangHyungDong <fire7009@naver.com>
2021-04-07 14:10:55 +0900

    디자인 오류 수정

--- onm-vue/src/components/dashboard/store/device-iotgw/iotgwInfo.vue
+++ onm-vue/src/components/dashboard/store/device-iotgw/iotgwInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-card>
       <iotgwInfo-query
         v-on:search="searchToIotGWInfo"
--- onm-vue/src/components/dashboard/store/device-sensor/sensorInfo.vue
+++ onm-vue/src/components/dashboard/store/device-sensor/sensorInfo.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-card>
       <sensorInfo-query
         v-on:search="searchToSensorInfo"
