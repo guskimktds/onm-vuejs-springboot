d61a354fb7adf2a9cf7b6f2c7df5c89bcf06a15c
DESKTOP-CQKCS6R\KangHyungDong <fire7009@naver.com>
2021-05-21 18:34:13 +0900

    메세지 중복 수정

--- onm-vue/src/components/dashboard/platform/dashboard/graph/camTransition.vue
+++ onm-vue/src/components/dashboard/platform/dashboard/graph/camTransition.vue
@@ -82,11 +82,11 @@ export default {
               this.loaded = true
 
             }else if(resCode==204){
-              alert('카메라 개통/해지 추이 데이터가 없습니다.')
+              alert(resMsg + '카메라 개통/해지 추이 데이터가 없습니다.')
             }
             else{
               this.datacollection = null;
-              alert(resCode + " / " + resMsg);
+              //alert(resCode + " / " + resMsg);
             }
           })
           .catch((ex) => {
