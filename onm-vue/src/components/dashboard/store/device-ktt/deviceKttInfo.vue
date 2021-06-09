0a3edf84d5ed43e3de06ebccb23e928755b31e44
DESKTOP-CQKCS6R\KangHyungDong <fire7009@naver.com>
2021-06-09 14:20:11 +0900

    ktt 단말 목록 ui 틀

--- onm-vue/src/components/dashboard/store/device-ktt/deviceKttInfo.vue
+++ onm-vue/src/components/dashboard/store/device-ktt/deviceKttInfo.vue
@@ -1,5 +1,154 @@
 <template>
-    <div>
-        테스트
-    </div>
+    <v-container fluid>
+        <v-card>
+            <device-ktt-info-query
+            v-on:search="searchdKTTInfo"
+            v-bind:param="searchParam"></device-ktt-info-query>
+
+            <device-ktt-info-list
+            v-bind:dkList="dkList"
+            v-bind:dkttPagingInfo="dkttPagingInfo"
+            @pagination="setToSearchParams">
+            </device-ktt-info-list>
+        </v-card>
+    </v-container>
 </template>
+
+<script>
+import DeviceKttInfoList from './deviceKttInfoList.vue'
+import DeviceKttInfoQuery from './deviceKttInfoQuery.vue'
+
+import EventBus from '../../../utils/common'
+import axios from "axios";
+
+const headers={
+  'User-Agent': 'GiGA Eyes (compatible;DeviceType/iPhone;DeviceModel/SCH-M20;DeviceId/3F2A009CDE;OSType/iOS;OSVersion/5.1.1;AppVersion/3.0.0;IpAddr/14.52.161.208)',
+  'Content-Type': 'application/json'
+}
+
+export default{
+    components:{
+        DeviceKttInfoList,
+        DeviceKttInfoQuery
+    },
+    data(){
+        return{
+            title: "KTT 단말정보 조회",
+            dkList:[],
+            reqpagingInfo:{
+                page_no:1,
+                view_cnt:10
+            },
+            dkttPagingInfo:{},
+            searchParam:{
+                cam_id:'',
+                user_id:'',
+                service_no:'',
+                status_code:''
+            }
+        }
+    },
+    methods: {
+        searchdKTTInfo: function(params){
+            var url=`${process.env.VUE_APP_BACKEND_SERVER_URL}/V110/ONM_13011/get_ktt_cam_list`
+
+            var reqParams=this.handleParams(params)
+
+            axios.post(url,reqParams,headers)
+                .then((response)=>{
+                    var resCode=response.data.res_code;
+                    var resMsg=response.data.res_msg;
+                    if(resCode==200){
+                        this.dkList=response.data.cam_list;
+                        this.dkttPagingInfo=response.data.paging_info
+                    }else if(resCode==204){
+                        this.dkList=[];
+                        this.dkttPagingInfo={};
+                        alert('KTT 단말 목록이 없습니다.')
+                    }else if(resCode==410){
+                        alert(resCode + " / " + resMsg);
+                        EventBus.$emit('top-path-logout');
+                        this.$store.dispatch("LOGOUT")
+                            .then( res => { 
+                            console.log(res.status)}).catch(({ message }) => (this.msg = message))
+                            this.$router.replace('/signin')
+                    }else{
+                        this.dkList=[];
+                        this.dkttPagingInfo={};
+                        alert(resCode+" / "+resMsg);
+                    }
+                })
+                .catch((ex)=>{
+                    console.log('조회 실패',ex)
+                })
+                .finally(function(){})
+        }
+    },
+
+    setToSearchParams: function(values){
+      console.log(values)
+
+      var params = {
+        page_no: values.page,
+        view_cnt: values.itemsPerPage
+      }
+
+      console.log(params)
+
+      this.searchdKTTInfo(params)
+    },
+
+    handleParams:function(params){
+      let newParams={}
+
+      if(params.page_no === undefined || params.page_no === ''){
+        newParams.page_no = this.reqPagingInfo.page_no
+      }else{
+        newParams.page_no = params.page_no
+      }
+      if(params.view_cnt === undefined || params.view_cnt === ''){
+        newParams.view_cnt = this.reqPagingInfo.view_cnt
+      }else{
+        newParams.view_cnt = params.view_cnt
+      } 
+
+      if(params.cam_id !== undefined && params.cam_id !== ''){
+        newParams.cam_id = params.cam_id
+      }else if(
+        this.searchParam.cam_id!==undefined&&
+        this.searchParam.cam_id!==""
+      ){
+        newParams.cam_id=this.searchParam.cam_id
+      }
+
+      if(params.user_id !== undefined && params.user_id !== ''){
+        newParams.user_id = params.user_id
+      }else if(
+        this.searchParam.user_id!==undefined&&
+        this.searchParam.user_id!==""
+      ){
+        newParams.user_id=this.searchParam.user_id
+      }
+
+      if(params.status_code !== undefined && params.status_code !== ''){
+        newParams.status_code = params.status_code
+      }else if(
+        this.searchParam.status_code!==undefined&&
+        this.searchParam.status_code!==""
+      ){
+        newParams.status_code=this.searchParam.status_code
+      }
+
+      if(params.service_no !== undefined && params.service_no !== ''){
+        newParams.service_no = params.service_no
+      }else if(
+        this.searchParam.service_no!==undefined&&
+        this.searchParam.service_no!==""
+      ){
+        newParams.service_no=this.searchParam.service_no
+      }
+
+      return newParams
+    }
+}
+</script>
---
+++ onm-vue/src/components/dashboard/store/device-ktt/deviceKttInfoList.vue
@@ -0,0 +1,93 @@
+<template>
+  <v-container
+      id="regular-tables"
+      fluid
+      tag="section"
+    >
+    <base-material-card
+        color="orange"
+        dark
+        icon="mdi-keyboard"
+        title="KTT 단말 목록 "
+        class="px-5 py-3"
+      >
+      <v-data-table
+        :headers="headers"
+        :items="dkList"
+        :options.sync="options"
+        :server-items-length="dkttPagingInfo.total_cnt"
+        class="elevation-1"
+        :footer-props="{itemsPerPageOptions:[5,10,15,20]}"
+        :header-props="{ sortIcon: null }"
+      >
+      <template v-slot:item.status_code="{item}">
+            <span>{{ switchString(item.status_code) }}</span>
+      </template>
+
+      </v-data-table>
+    </base-material-card>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: ['dkList','dkttPagingInfo'],
+  data() {
+    return {
+      last: 0,
+      dialog: false,
+      dialogDelete: false,
+      editedIndex: -1,
+      options: {},
+      totalList: 0,
+      loading: true,
+      headers: [
+        {
+          text: "카메라ID",
+          align: "start",
+          sortable: false,
+          value: "cam_id",
+        },
+        { text: "매장ID", value: "user_id" },
+        { text: "서비스번호", value: "service_no" },
+        { text: "상태 코드", value: "status_code" },
+        { text: "KTT 계약 ID", value: "reg_date" },
+      ],
+    };
+  },
+   methods: {
+    getDataFromApi() {
+      this.loading = true;
+      this.$emit("pagination", this.options);
+    },
+    switchString(values){
+        if(values=='D'){
+            return '삭제'
+        }else if(values=='S'){
+            return '정상'
+        }
+    }
+    
+  },
+
+  watch: {
+    options: {
+      handler() {
+        this.getDataFromApi();
+      },
+      deep: true,
+    },
+  },
+  updated() {
+      if(this.last!==this.kttPagingInfo.total_cnt){
+        this.options.page=1
+      }
+      if(this.kttPagingInfo.total_cnt!==undefined){
+      this.last=this.kttPagingInfo.total_cnt
+      }
+  },
+    
+}
+</script>
+<style>
+</style>
