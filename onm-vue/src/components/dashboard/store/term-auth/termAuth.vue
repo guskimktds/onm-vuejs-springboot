21f92668cd02a88f465e725e7dcca5375cc2722c
guskimktds <gusraccoon@gmail.com>
2021-11-17 19:36:57 +0900

    alert 제거

--- onm-vue/src/components/dashboard/store/term-auth/termAuth.vue
+++ onm-vue/src/components/dashboard/store/term-auth/termAuth.vue
@@ -83,11 +83,7 @@ export default {
           }else{
             this.pList = [];
             this.resPagingInfo = {};
-<<<<<<< HEAD
             console.log(resCode + " / " + resMsg);
-=======
-            alert("Error");
->>>>>>> develop
           }
         })
         .catch((ex) => {
