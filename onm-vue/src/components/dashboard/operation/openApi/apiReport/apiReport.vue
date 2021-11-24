3834fd009a9a5d09860a61048ee9478cfa22f861
sang wook <wook@eesolution.co.kr>
2021-11-24 18:36:30 +0900

    EventBus handling

--- onm-vue/src/components/dashboard/operation/openApi/apiReport/apiReport.vue
+++ onm-vue/src/components/dashboard/operation/openApi/apiReport/apiReport.vue
@@ -4,7 +4,7 @@
         <count-api-tab></count-api-tab>
           <!-- v-on:search="searchToGraph" -->
           <api-report-query
-          v-bind:param=param>
+            v-bind:param="param">
           </api-report-query>    
           <api-graph
           v-bind:param="checkDate(param)"
@@ -39,6 +39,7 @@ import apiReportQuery from './apiReportQuery'
 import CountApiTab from './countApiTab.vue'
 import dateInfo from "../../../../utils/common"
 import ApiGraph from './apiGraph.vue'
+import EventBus from '../../../../../EventBus'
 // import ServiceCount from './serviceCount.vue'
 // import ApiCount from './apiCount.vue'
 // import axios from "axios"
@@ -60,18 +61,21 @@ export default {
     }
   },
   methods: {   
-        checkDate: function(value){
-            let newParams={}
-            if(Number(value.start_date.replace(/-/g,""))-Number(value.end_date.replace(/-/g,""))>0){
-                alert('형식에 맞는 날짜 검색값을 입력해주세요')
-                this.param.start_date=dateInfo().threeMonthDashFormat
-                this.param.end_date=dateInfo().currentDateDashFormat
-            }else{
-                newParams=value
-            }
-            return newParams;
-        },
-  
+    checkDate: function(value){
+      let newParams={}
+      if(Number(value.start_date.replace(/-/g,""))-Number(value.end_date.replace(/-/g,""))>0){
+          alert('형식에 맞는 날짜 검색값을 입력해주세요')
+          this.param.start_date=dateInfo().threeMonthDashFormat
+          this.param.end_date=dateInfo().currentDateDashFormat
+      }else{
+          newParams=value
+      }
+      return newParams;
+    },
+
+    searchAllGraph: function(){
+      EventBus.$emit('search-graph', this.param);
+    }
   }
 }
 </script>
