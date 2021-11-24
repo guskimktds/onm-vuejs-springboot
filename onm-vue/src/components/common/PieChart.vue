8c713fd27cae44eea0b928a33557f3c35f2aa52c
HEO1122 <sk731@naver.com>
2021-11-24 14:59:37 +0900

    차트 커밋

--- onm-vue/src/components/common/PieChart.vue
+++ onm-vue/src/components/common/PieChart.vue
@@ -9,20 +9,22 @@ export default {
   mounted () {
     // this.chartData is created in the mixin.
     // If you want to pass options please create a local options object
-   this.renderChart({
-      // labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
-      datasets: [
-        // {
-        //   backgroundColor: [
-        //     '#41B883',
-        //     '#E46651',
-        //     '#00D8FF',
-        //     '#DD1B16'
-        //   ],
-        //   data: [40, 20, 80, 10]
-        // }
-      ]
-    }, {responsive: true, maintainAspectRatio: false})
+   this.renderChart(this.chartData,
+    //  {
+    //   labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+    //   datasets: [
+    //     {
+    //       backgroundColor: [
+    //         '#41B883',
+    //         '#E46651',
+    //         '#00D8FF',
+    //         '#DD1B16'
+    //       ],
+    //       data: [40, 20, 80, 10]
+    //     }
+    //   ]
+    // }, 
+    {responsive: true, maintainAspectRatio: false})
   }
 }
 </script>
