eaa9729b67fd4d2fda4aa9542f7b93d794452dc7
DESKTOP-CQKCS6R\KangHyungDong <fire7009@naver.com>
2021-12-06 15:14:34 +0900

    res_msg 삭제

--- onm-vue/src/components/dashboard/platform/dashboard/graph/iotTransition.vue
+++ onm-vue/src/components/dashboard/platform/dashboard/graph/iotTransition.vue
@@ -59,7 +59,6 @@ export default {
           .then((response) => {
             console.log(response.data)
             var resCode = response.data.res_code;
-            var resMsg = response.data.res_msg;
             if(resCode == 200){
               this.labels = this.getLabels(response.data.data.date_list)
               this.datacollection = {
