eb42d93dca1163942f192200a51f043cd18d43e9
HwangSangWook <wook@eesolution.co.kr>
2021-12-14 16:49:30 +0900

    Revert "resolve slow compile issue that HTML template in dev mode at subInfo.vue"
    This reverts commit 76f55ea1aeee5acdf38ddb33ee80f42a9444c29a.

--- onm-vue/vue.config.js
+++ onm-vue/vue.config.js
@@ -4,12 +4,6 @@ module.exports = {
     host: 'sub.ktvsaas.co.kr',
     port : 8443,
     https: true
-  },
-  configureWebpack: function(config) {
-    if(process.env.NODE_ENV === 'local'){
-      config.output.filename = 'js/[name].[hash].js'
-      config.output.chunkFilename = 'js/[name].[hash].js'
-    }
   }
   
 }
